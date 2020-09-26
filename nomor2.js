22036d611ec12000cdaa3775291a4c40eaed4992
AlQasmSystem <alqasmsystem@localhost.localdomain>
2020-09-26 13:58:43 +0000

    Bismillah, Nomor 2

---
+++ nomor2.js
@@ -0,0 +1,10 @@
+var nilaiSantri = [50, 90, 65, 67, 89, 80, 50, 97, 84];
+
+var total = 0;
+var bykNilai = nilaiSantri.length;
+
+for (i = 0; i < bykNilai; i++) {
+  total += nilaiSantri[i];
+}
+
+document.write(total / bykNilai);
